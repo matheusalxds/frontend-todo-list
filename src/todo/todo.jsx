7cc7a8ff3c6624d91281f3b522b03576abc3a9ab
matheus <freeuser153>
2017-11-27 22:26:26 -0200

    Added components, first insert hitting in the api

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
 import PageHeader from '../template/pageHeader';
 import TodoForm from './todoForm';
 import TodoList from './todoList';
 
+const URL = 'http://localhost:3003/api/todos';
+
 export default class Todo extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      description: '',
+      list: [],
+    };
+
+    this.handleAdd = this.handleAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event){
+    this.setState({...this.state, description: event.target.value });
+  }
+  handleAdd(){
+    const description = this.state.description;
+    axios.post(URL, { description })
+         .then(resp => console.log('Working'));
+  }
   render(){
     return (
       <div>
@@ -11,7 +34,11 @@ export default class Todo extends Component{
           name='Task'
           small='Create'
         />
-        <TodoForm />
+        <TodoForm
+          handleAdd={this.handleAdd}
+          description={this.state.description}
+          handleChange={this.handleChange}
+        />
         <TodoList />
       </div>
     );
